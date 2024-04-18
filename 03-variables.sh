70e591decab2083290b9eb741db2136202fbd726
Nagajyothipidugu <9pidugujyothi@gmail.com>
2024-04-18 18:20:16 +0530

    simp-conversation

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+person1=Ramesh
+person2=Suresh 
+
+echo "$person1: : How are you ?"
+echo "$person2: : I am doging good, how about you?"
+echo  $person1: : I am working as a devops engineer 
+echo  $person2: : ok good, catch you later 
