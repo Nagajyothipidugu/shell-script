4b3ac9126832904648a2bab12fd1bc3d318bd880
Nagajyothipidugu <9pidugujyothi@gmail.com>
2024-04-18 18:44:49 +0530

    pass-arguments

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+echo "Enter your username:" 
+
+read  USERNAME #username is variable here 
+
+echo "Enter your password:" 
+
+read PASSWORD  
+
+echo "user name is $USERNAME , password is $PASSWORD 
