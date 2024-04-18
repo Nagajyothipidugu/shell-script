6ff57910da8f86e758a1f620aa3e88ed2dc3c5d2
Nagajyothipidugu <9pidugujyothi@gmail.com>
2024-04-18 18:31:39 +0530

    pass-arguments

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+person1=$1
+person2=$2
+
+echo "$person1: : How are you ?"
+echo "$person2: : I am doging good, how about you?"
+echo  $person1: : I am working as a devops engineer 
+echo  $person2: : ok good, catch you later 
