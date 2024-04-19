be484a33bdc4bdd4c45239382227c307af3a16e3
Nagajyothipidugu <9pidugujyothi@gmail.com>
2024-04-19 20:11:40 +0530

    some-message

--- 09-condition.sh
+++ 09-condition.sh
@@ -4,8 +4,7 @@ NUMBER=$1
 
 if [$NUMBER -gt 10] 
 then 
-   echo "Print  the given $NUMBER is greater than 10" 
+   echo " The given $NUMBER is greater than 10" 
 else 
-  echo "Print the given $NUMBER is less than 10"   
+  echo "The  given $NUMBER is less than 10"   
  fi 
-  
