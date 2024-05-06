3b780bf05c2e42dcda3667a127e58781fd0c8fbc
Nagajyothipidugu <9pidugujyothi@gmail.com>
2024-05-06 17:27:57 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$($0 |cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 
@@ -24,9 +24,9 @@ else
  echo "You are a super user " 
 fi  
 
-dnf install mysql -y 
+dnf install mysql -y  &>>LOGFILE
 VALIDATE $? "Installation of mysql" 
 
-dnf install git -y 
+dnf install git -y  &>>LOGFILE
 VALIDATE $? "INSTALLATION OF GIT"
   
