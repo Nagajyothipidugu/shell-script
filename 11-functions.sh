8dcbaf521079f521eea002dfccc7f6ab5c714c5d
Nagajyothipidugu <9pidugujyothi@gmail.com>
2024-05-06 16:58:38 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,11 +1,16 @@
 #!/bin/bash 
 
-USERID=$( id -u ) 
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$($0 |cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
 
 VALIDATE(){
   if [ $1 -ne 0 ] 
   then 
    echo "$2 is ....FAILURE" 
+   exit 1
   else 
    echo "$2 is.... SUCCESS" 
   fi  
